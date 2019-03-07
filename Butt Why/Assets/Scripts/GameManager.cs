2d4142cd55814b6438f97f10b362ab23547af253
Weltor <kamies132779@gmail.com>
2019-03-07 15:04:05 +0100

    Bouncer collider en spatie werken nu en het menu werkt. Next knop is voor de animatie aan het eind

--- Butt Why/Assets/Scripts/GameManager.cs
+++ Butt Why/Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ public class GameManager : MonoBehaviour
 {
     public GameObject bustedUI;
 
+    public GameObject bouncer;
+
+    //int bouncerDelay = 2000;
+
     public void busted ()
     {
         bustedUI.SetActive(true);
@@ -13,4 +17,13 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Busted");
     }
+
+    //public void bouncerInput()
+    //{
+        //bouncer.GetComponent<BouncerMovement>().enabled = false;
+
+        
+
+        //bouncer.GetComponent<BouncerMovement>().enabled = true;
+    //}
 }
--- Butt Why/Assets/Scripts/TriggerEvent.cs
+++ Butt Why/Assets/Scripts/TriggerEvent.cs
@@ -4,12 +4,39 @@ public class TriggerEvent : MonoBehaviour
 {
     public GameManager gameManager;
 
+    public bool spacePressed = false;
+
+    public bool inCollider = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            inCollider = true;
+        }
+        else inCollider = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            spacePressed = true;
+        }
+        else
+        {
+            spacePressed = false;
+        }
+
+        if (inCollider == true && spacePressed == true)
+        {
             gameManager.GameOver();
             gameManager.busted();
         }
+
+        //if (inCollider == false && spacePressed == true)
+        //{
+            //gameManager.bouncerInput();
+        //}
     }
 }
